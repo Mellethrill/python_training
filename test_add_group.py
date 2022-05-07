5502d7883135b5c78d8a039efa64a41a08006be4
Mellethrill <sadslov@yandex.ru>
2022-05-07 21:51:57 +0300

    Фикстура + переехали на pytest. Задание 4.

--- test_add_group.py
+++ test_add_group.py
@@ -1,25 +1,7 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 from group import Group
 from application import Application
-import unittest
 import pytest
-
-def is_element_present(self, how, what):
-    try:
-        self.wd.find_element(by=how, value=what)
-    except NoSuchElementException as e:
-        return False
-    return True
-
-def is_alert_present(self):
-    try:
-        self.wd.switch_to_alert()
-    except NoAlertPresentException as e:
-        return False
-    return True
 
 
 @pytest.fixture
@@ -39,6 +21,3 @@ def test_add_empty_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
     app.logout()
-
-if __name__ == "__main__":
-    unittest.main()
