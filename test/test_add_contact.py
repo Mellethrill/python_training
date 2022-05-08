c9aeb69a1713e86590d175383fa086ecc4c1231b
Mellethrill <sadslov@yandex.ru>
2022-05-08 20:53:11 +0300

    Улучшение фикстуры, ч.1

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,7 +2,5 @@
 from model.contact import Contact
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(firstname="Denis", middlename="Alex", lastname="Ratkevich", nickname="Mels", title="Title1", company="Cryptex", address="213231 AVS 21", phone_home="12345678",
                             phone_mobile="87654321", phone_work="11-11", phone_fax="22-22", email1="123@ya.ru", email2="321@ya.com", email3="231@ya.net", homepage="www.123.ru", address2="123342 sdf @@", phone2="123345", notes="done?"))
-    app.session.logout()
--- test/test_modification_contact.py
+++ test/test_modification_contact.py
@@ -2,7 +2,5 @@ from model.contact import Contact
 
 
 def test_modification_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modification_first_contact(Contact(firstname="upd1", middlename="upd1", lastname="upd1", nickname="upd1", title="upd1", company="upd1", address="upd1", phone_home="1",
                             phone_mobile="1", phone_work="1", phone_fax="1", email1="upd1@ya.ru", email2="upd1@ya.com", email3="upd1@ya.net", homepage="www.upd1.ru", address2="upd1", phone2="upd1", notes="upd1"))
-    app.session.logout()
