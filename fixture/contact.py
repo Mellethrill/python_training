40cb3af0557ac739ce7f99200d7721af8a89795f
Mellethrill <sadslov@yandex.ru>
2022-05-13 18:58:52 +0300

    Добавил 2 новых теста на проверку телефонов.

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,5 @@
 from model.contact import Contact
+import re
 
 class ContactHelper:
 
@@ -99,10 +100,53 @@ class ContactHelper:
             self.open_contact_page()
             self.contact_cache = []
             for element in wd.find_elements_by_css_selector("tr[name]"):
+                cells = element.find_elements_by_tag_name("td")
                 id = element.find_element_by_name("selected[]").get_attribute("value")
                 last_name = element.find_element_by_css_selector("tr > td:nth-child(2)").text
                 first_name = element.find_element_by_css_selector("tr > td:nth-child(3)").text
-                self.contact_cache.append(Contact(id=id, lastname=last_name, firstname=first_name))
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(Contact(id=id, lastname=last_name, firstname=first_name, phone_home=all_phones[0],
+                                                  phone_mobile=all_phones[1], phone_work=all_phones[2], phone2=all_phones[3]))
         return list(self.contact_cache)
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.open_contact_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_contact_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        phone_home = wd.find_element_by_name("home").get_attribute("value")
+        phone_work = wd.find_element_by_name("work").get_attribute("value")
+        phone_mobile = wd.find_element_by_name("mobile").get_attribute("value")
+        phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, id=id, phone_home=phone_home, phone_work=phone_work, phone_mobile=phone_mobile, phone2=phone2)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        phone_home = re.search("H: (.*)", text).group(1)
+        phone_work = re.search("W: (.*)", text).group(1)
+        phone_mobile = re.search("M: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Contact(phone_home=phone_home, phone_work=phone_work,
+                       phone_mobile=phone_mobile, phone2=phone2)
+
+
+
 
 
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,7 +4,7 @@ from model.contact import Contact
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
     contact = Contact(firstname="Denis", middlename="Alex", lastname="Ratkevich", nickname="Mels", title="Title1", company="Cryptex", address="213231 AVS 21", phone_home="12345678",
-                            phone_mobile="87654321", phone_work="11-11", phone_fax="22-22", email1="123@ya.ru", email2="321@ya.com", email3="231@ya.net", homepage="www.123.ru", address2="123342 sdf @@", phone2="123345", notes="done?")
+                            phone_mobile="87654321", phone_work="1111", phone_fax="2222", email1="123@ya.ru", email2="321@ya.com", email3="231@ya.net", homepage="www.123.ru", address2="123342 sdf @@", phone2="123345", notes="done?")
     app.contact.create_contact(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
