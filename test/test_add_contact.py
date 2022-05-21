31f7146f4586710c184b576a2511544ea3b4d341
Mellethrill <sadslov@yandex.ru>
2022-05-21 18:09:53 +0300

    Задание 18 и 19, Done.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,21 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [Contact(firstname=random_string("firstname", 7), lastname=random_string("lastname", 10), company=random_string("company", 10),
-                    address=random_string("address", 13), phone_home=random_string("home", 6), phone_work=random_string("work", 7),
-                    email1=random_string("email1", 7), homepage=random_string("homepage", 7), notes=random_string("notes", 7))
-    for i in range(5)]
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create_contact(contact)
     assert len(old_contacts) + 1 == app.contact.count()
